ead5d15dfd6197916c4845cda5a2b74b253e1c72
ZerRazer <christian.varyvoda@web.de>
2023-03-05 09:59:59 +0100

    SineWave going outside of the cube fixed, 4 LEDs in the lower centre no longer light up continuously

--- OwnCube/SineWave.cpp
+++ OwnCube/SineWave.cpp
@@ -7,7 +7,7 @@
 #define CENTER_Z 3.5
 
 // define number of iterations
-#define ITERATIONS 400
+#define ITERATIONS 185
 class SineWave{
   private:
     GenerationMethods gen;
@@ -37,13 +37,20 @@ class SineWave{
                 double distance = sqrt(dx * dx + dy * dy) * 5;              ;
 
                 // calculate z coordinate using sin() based on distance from center and iteration counter
-                double z = CENTER_Z + sin((distance + i) * 0.1 ) * 4;
+                double z = CENTER_Z + sin((distance + i) * 0.1 ) * 3.5;
                 // cast coordinates to integers and set voxel in cube
                 // determine the position in the cube array based on the smoothed x, y, and z coordinates
                 double smoothX = x;
                 double smoothY = y;
                 double smoothZ = z;
-            
+                if(z > 7){
+                  smoothZ = 7;
+                }
+                else{
+                  smoothZ = z;
+                }
+                
+
                 // check if the smoothed position is within the bounds of the cube
                 if (smoothX >= 0 && smoothX < 8 && smoothY >= 0 && smoothY < 8 && smoothZ >= 0 && smoothZ < 8) {
                 // set the voxel at the smoothed position in the cube
