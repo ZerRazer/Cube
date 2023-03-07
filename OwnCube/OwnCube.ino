ca569f20b7d9438fcff3742507802297eee89faf
ZerRazer <christian.varyvoda@web.de>
2023-03-07 22:16:39 +0100

    Interrupt Pin and Debouncer added

--- OwnCube/OwnCube.ino
+++ OwnCube/OwnCube.ino
@@ -5,7 +5,7 @@
 #include "CubeJump.h"
 #include "GenerationMethods.h"
 //#include "RotatingSineWave.h"
-#define BUTTON_PIN 5
+#define BUTTON_PIN 2
 #define TOTAL_EFFECTS 3
 
 #define SINEWAVE 0
@@ -16,36 +16,39 @@ GenerationMethods gen;
 SineWave sine = new SineWave(false);
 PlaneBoing plane;
 CubeJump cube;
-
-
-
 //RotatingSineWave rotateSin;
-
 uint8_t currentEffect;
+int timer = 0;
 void setup() {
   SPI.begin();
   SPI.beginTransaction(SPISettings(8000000, MSBFIRST, SPI_MODE0));
   Serial.begin(9600);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
-  currentEffect = 2;
-}
 
-void loop() {
-  //Serial.println("Loop Anfang");
-  if (digitalRead(BUTTON_PIN) == LOW) {
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), change_mode, CHANGE);
+  currentEffect = 0;
+}
+void change_mode(){
+  //Debouncer
+  if(millis() - timer> 1000){
+    timer = 0;
+  }
+  if(timer == 0){
+    timer = millis();
     Serial.println("KNOPF GEDRÜCKT");
     gen.clearCube();
     gen.loading = true;
     gen.timer = 0;
     currentEffect++;
-    if (currentEffect == TOTAL_EFFECTS) {
+    if (currentEffect == 2) {
       currentEffect = 0;
     }
   }
+}
+void loop() {
   switch (currentEffect) {
     case SINEWAVE: sine.drawSineWave(); break;
     case PLANEBOING: plane.drawPlaneBoing(); break;
-    case CUBEJUMP: cube.drawCubeJump(); break;
     default: sine.drawSineWave();
   }
 }
